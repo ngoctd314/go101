93b1a3c119d4ee5affe6f30f82a0b00c92a779fb
tranngoc <tranngoc.contact@gmail.com>
2022-08-15 21:53:17 +0700

    u

--- main.go
+++ main.go
@@ -1,9 +1,31 @@
 package main
 
-import "github.com/ngoctd314/go101/go-code/golog"
+import (
+	"strconv"
+
+	"github.com/ngoctd314/go101/go-code/golog"
+)
 
 func main() {
-	golog.Info(golog.Sys, "hello world")
-	golog.Warn(golog.Sys, "hello world")
-	golog.Error(golog.Sys, "hello world")
+	fn := func() func() int {
+		count := 0
+		return func() int {
+			count++
+			return count
+		}
+	}
+	k := fn()
+	golog.Info(golog.Sys, strconv.Itoa(k()))
+	golog.Info(golog.Sys, strconv.Itoa(k()))
+	golog.Info(golog.Sys, strconv.Itoa(k()))
+	golog.Info(golog.Sys, strconv.Itoa(k()))
+
+	var a = 1
+	switch a {
+	case 1:
+		golog.Info(golog.Sys, strconv.Itoa(a))
+		fallthrough
+	case 2:
+		golog.Info(golog.Sys, strconv.Itoa(a))
+	}
 }

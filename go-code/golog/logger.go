807399c21ec392155aef641ad673b9b3c9d296dc
ngoctd <trangoc.contact@gmail.com>
2022-08-22 15:19:58 +0700

    u

--- go-code/golog/logger.go
+++ go-code/golog/logger.go
@@ -1,7 +1,5 @@
 package golog
 
-import "sync"
-
 // log_factory: combine writer, encoder and lib => golog
 //
 
@@ -19,7 +17,6 @@ type Logger interface {
 
 var (
 	logMgmt = make(map[LogType]Logger)
-	rwm     sync.RWMutex
 )
 
 func init() {
@@ -30,54 +27,42 @@ func init() {
 
 // Info log with info level
 func Info(logType LogType, msg string) {
-	rwm.RLock()
 	log := logMgmt[logType]
-	rwm.RUnlock()
 
 	log.Infow(msg, "type", logType)
 }
 
 // Infow log with info level, with key-value pair as option
 func Infow(logType LogType, msg string, kv ...any) {
-	rwm.RLock()
 	log := logMgmt[logType]
-	rwm.RUnlock()
 
 	log.Infow(msg, kv...)
 }
 
 // Warn log with warn level
 func Warn(logType LogType, msg string) {
-	rwm.RLock()
 	log := logMgmt[logType]
-	rwm.RUnlock()
 
 	log.Warnw(msg, "type", logType)
 }
 
 // Warnw log with warn level, with key-value pair as option
 func Warnw(logType LogType, msg string, kv ...any) {
-	rwm.RLock()
 	log := logMgmt[logType]
-	rwm.RUnlock()
 
 	log.Warnw(msg, kv...)
 }
 
 // Error log with error level
 func Error(logType LogType, msg string) {
-	rwm.RLock()
 	log := logMgmt[logType]
-	rwm.RUnlock()
 
 	log.Errorw(msg, "type", logType)
 }
 
 // Errorw log with error level, with key-value pair as option
 func Errorw(logType LogType, msg string, kv ...any) {
-	rwm.RLock()
 	log := logMgmt[logType]
-	rwm.RUnlock()
 
 	log.Errorw(msg, kv...)
 }

108449fb9fdb15c45faef457418e194de1a5c39c
ngoctd <trangoc.contact@gmail.com>
2022-09-05 08:58:25 +0700

    channel usecase

--- main.go
+++ main.go
@@ -1,19 +1,37 @@
 package main
 
-<<<<<<< HEAD
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func longTimeRequest() <-chan int32 {
+	r := make(chan int32)
+
+	go func() {
+		// Simulate a workload
+		time.Sleep(time.Second * 5)
+		r <- rand.Int31n(100)
+	}()
+
+	return r
+}
+
+func sumSquares(a, b int32) int32 {
+	return a*a + b*b
+}
 
 func main() {
-	var k int = 0
-	if 1 == 1 {
-		k := 1
-		fmt.Println(k)
-	} else {
-		k := 2
-		fmt.Println(k)
-	}
-	fmt.Println(k)
-=======
-func main() {
->>>>>>> f2daed86771bed2cf14d91bc3a59c19cbb042d5e
+	n := time.Now()
+	rand.Seed(time.Now().UnixNano())
+
+	a, b := fn(longTimeRequest(), longTimeRequest())
+
+	fmt.Println(sumSquares(a, b))
+	fmt.Println(time.Since(n).Seconds())
+}
+
+func fn(ch1 <-chan int32, ch2 <-chan int32) (int32, int32) {
+	return <-ch1, <-ch2
 }

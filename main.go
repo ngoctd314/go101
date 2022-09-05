e5ba6d9661141634409d6ae92654abe8b4e263fd
ngoctd <trangoc.contact@gmail.com>
2022-09-05 19:01:40 +0700

    u

--- main.go
+++ main.go
@@ -6,32 +6,21 @@ import (
 	"time"
 )
 
-func longTimeRequest() <-chan int32 {
-	r := make(chan int32)
-
-	go func() {
-		// Simulate a workload
-		time.Sleep(time.Second * 5)
-		r <- rand.Int31n(100)
-	}()
-
-	return r
-}
-
-func sumSquares(a, b int32) int32 {
-	return a*a + b*b
+func source(c chan<- int32) {
+	rb := rand.Intn(3) + 1
+	time.Sleep(time.Duration(rb) * time.Second)
+	select {
+	case c <- int32(rb):
+	default:
+	}
 }
 
 func main() {
-	n := time.Now()
 	rand.Seed(time.Now().UnixNano())
-
-	a, b := fn(longTimeRequest(), longTimeRequest())
-
-	fmt.Println(sumSquares(a, b))
-	fmt.Println(time.Since(n).Seconds())
-}
-
-func fn(ch1 <-chan int32, ch2 <-chan int32) (int32, int32) {
-	return <-ch1, <-ch2
+	c := make(chan int32, 1)
+	for i := 0; i < 5; i++ {
+		go source(c)
+	}
+	rnd := <-c
+	fmt.Println(rnd)
 }

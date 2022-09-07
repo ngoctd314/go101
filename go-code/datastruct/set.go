0ef1d99a91ba2a8daa858d2bdd8fc437d8f39f4b
ngoctd <trangoc.contact@gmail.com>
2022-09-07 17:54:21 +0700

    add set

--- go-code/datastruct/set.go
+++ go-code/datastruct/set.go
@@ -1,17 +1,19 @@
 package datastruct
 
-import "strings"
-
 // Set datastruct solving for unique problem
 type Set interface {
 	// Exist checks a key is in Set
-	Exist(string) bool
-	// Has checks multi key is in Set
-	Has(...string) bool
-	// Add inserts multi key into set
+	Exist(...string) bool
+	// Evict deletes matching key in Set
+	Evict(...string)
+	// Add inserts multi key into Set
 	Add(...string)
 	// String convert set key => string. Use , as separator
-	String() string
+	String() []string
+	// Intersection intersect other set into current set
+	Intersection(Set)
+	// Union union other set into current set
+	Union(Set)
 }
 
 type set map[string]struct{}
@@ -31,8 +33,13 @@ func (s set) Add(keys ...string) {
 }
 
 // Get implements Set
-func (set) Exist(key string) bool {
-	panic("unimplemented")
+func (s set) Exist(keys ...string) bool {
+	for i := 0; i < len(keys); i++ {
+		if _, ok := s[keys[i]]; !ok {
+			return false
+		}
+	}
+	return true
 }
 
 // Has implements Set
@@ -41,10 +48,31 @@ func (set) Has(...string) bool {
 }
 
 // String implements Set
-func (s set) String() string {
+func (s set) String() []string {
 	str := []string{}
 	for k := range s {
 		str = append(str, k)
 	}
-	return strings.Join(str, ",")
+	return str
+}
+
+// Intersection implements Set
+func (s set) Intersection(otherSet Set) {
+	for k := range s {
+		if !otherSet.Exist(k) {
+			s.Evict(k)
+		}
+	}
+}
+
+// Union implements Set
+func (s set) Union(otherSet Set) {
+	s.Add(otherSet.String()...)
+}
+
+// Evict implements Set
+func (s set) Evict(keys ...string) {
+	for i := 0; i < len(keys); i++ {
+		delete(s, keys[i])
+	}
 }

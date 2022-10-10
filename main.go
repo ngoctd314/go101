acffbd925ac1921bd714768244b865ffc060252b
ngoctd <trangoc.contact@gmail.com>
2022-10-10 18:29:37 +0700

    u

--- main.go
+++ main.go
@@ -1,36 +1,69 @@
 package main
 
-import (
-	"log"
-	"math/rand"
-	"time"
-)
-
-// Seat ...
+// Seat ..
 type Seat int
 
-// Bar ...
-type Bar chan Seat
-
-// ServeCustomerAtSeat ...
-func (bar Bar) ServeCustomerAtSeat(c int, seat Seat) {
-	log.Print("++ customer#", c, " drinks at seat#", seat)
-	time.Sleep(time.Second * time.Duration(2+rand.Intn(6)))
-	log.Print("-- customer#", c, " frees seat#", seat)
-	bar <- seat
-}
+// Bar ..
+type Bar Seat
 
 func main() {
-	rand.Seed(time.Now().UnixNano())
-
-	bar27x7 := make(Bar, 10)
-	for seatId := 0; seatId < cap(bar27x7); seatId++ {
-		bar27x7 <- Seat(seatId)
-	}
-
-	for customerId := 0; ; customerId++ {
-		time.Sleep(time.Second)
-		seat := <-bar27x7
-		go bar27x7.ServeCustomerAtSeat(customerId, seat)
-	}
 }
+
+// var maxCCU = 10
+// var consumers = make(chan consumer)
+// var hc = httpclient.NewClient()
+
+// type consumer struct {
+// 	resp chan httpclient.Response
+// 	args httpclient.Args
+// }
+
+// func serveHTTPClient(consumers chan consumer) {
+// 	for consum := range consumers {
+// 		resp := hc.Do(context.TODO(), consum.args)
+// 		consum.resp <- resp
+// 	}
+// }
+
+// func init() {
+// 	for i := 0; i < maxCCU; i++ {
+// 		go serveHTTPClient(consumers)
+// 	}
+// }
+
+// func main() {
+// 	http.HandleFunc("/mem", func(w http.ResponseWriter, r *http.Request) {
+// 		time.Sleep(time.Second * 5)
+
+// 		w.Write([]byte("mem"))
+// 	})
+
+// 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+// 		now := time.Now()
+// 		rs := []string{}
+// 		l := make([]chan httpclient.Response, 0)
+// 		for i := 0; i < 5; i++ {
+// 			resp := make(chan httpclient.Response)
+// 			args := httpclient.Args{
+// 				URL:    "http://localhost:8001",
+// 				Method: http.MethodGet,
+// 			}
+// 			consumers <- consumer{
+// 				resp: resp,
+// 				args: args,
+// 			}
+// 			l = append(l, resp)
+// 		}
+// 		for _, v := range l {
+// 			b := <-v
+// 			c := b.Body
+// 			rs = append(rs, string(c))
+// 		}
+
+// 		fmt.Println("GET /:", time.Since(now))
+
+// 		w.Write([]byte(strings.Join(rs, "\t")))
+// 	})
+
+// 	http.ListenAndServe(":8080", nil)
+// }

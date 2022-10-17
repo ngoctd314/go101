b57dd34e73e4be032571691a3879ffd1e5dcbef1
tranngoc <tranngoc.contact@gmail.com>
2022-10-17 21:54:32 +0700

    u

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
 
 func main() {
-	os.Create("./logs/test.txt")
+	os.MkdirAll("./logs/test", 0777)
+	os.MkdirAll("./logs/test1", 0777)
+	f, err := os.Create("./logs/test/test.txt")
+	f.Close()
+
+	os.Rename(f.Name(), "./logs/test1/teset.txt")
+	fmt.Println(f.Name(), err, filepath.Base(f.Name()))
 }

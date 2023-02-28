3f2cb7b8821ba526faa9462263b2ee5af9aece2c
tranngoc <tranngoc.contact@gmail.com>
2023-02-28 23:37:41 +0700

    u

--- main.go
+++ main.go
@@ -1,49 +1,17 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"log"
-	"net/http"
-	"time"
-
-	"github.com/ngoctd314/go101/go-code/httpclient"
 )
 
 func main() {
-	hc := httpclient.NewClient(
-		httpclient.WithMaxConns(100),
-		httpclient.WithMaxKeepAliveIdleDuration(time.Second*10),
-		httpclient.WithDialFunc(1),
-	)
-
-	ctx := context.Background()
-
-	resp := hc.DoMany(ctx,
-		httpclient.Args{
-			URL:    "http://localhost:8080",
-			Method: http.MethodGet,
-		},
-		httpclient.Args{
-			URL:    "http://localhost:8080",
-			Method: http.MethodGet,
-		},
-		httpclient.Args{
-			URL:    "http://localhost:8080",
-			Method: http.MethodGet,
-		},
-		httpclient.Args{
-			URL:    "http://localhost:8080",
-			Method: http.MethodGet,
-		},
-	)
-
-	for v := range resp {
-		if v.Err != nil {
-			log.Panic(v.Err)
-		}
-		fmt.Println(string(v.Body))
-	}
-	time.Sleep(time.Second * 1)
+	ch := make(chan *int)
+	go func() {
+		p := 10
+		ch <- &p
+		fmt.Printf("%p\n", &p)
+	}()
+	v := <-ch
+	fmt.Printf("%p\n", v)
 
 }
